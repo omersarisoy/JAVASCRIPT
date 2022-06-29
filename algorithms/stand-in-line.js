7a360512a25f4ad43fa4f1945ac4d9cb10290ced
Ömer Sarısoy <omer.sarisoy@asnak.com>
2022-06-29 10:11:53 +0300

    js

--- algorithms/stand-in-line.js
+++ algorithms/stand-in-line.js
@@ -13,3 +13,8 @@ const testArr = [3,4,5,6];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr,1));
 console.log("After: " + JSON.stringify(testArr));
+
+// JAVASCRIPT git:(main) ✗ ls        
+// README.md  algorithms entrance   index.html index.js
+// ➜  JAVASCRIPT git:(main) ✗ cd algorithms 
+// ➜  algorithms git:(main) ✗ node stand-in-line.js 
